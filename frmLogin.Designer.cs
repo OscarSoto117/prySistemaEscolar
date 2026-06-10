9ba425b668d7d18ff26276f499aa8212e10e9076
OscarSoto117 <20251013@uthh.edu.mx>
2026-06-10 01:35:22 -0600

    Agregar archivos de proyecto.

---
+++ frmLogin.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace prySistemaEscolar
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            pcbLoginFondo = new PictureBox();
+            txtUsuario = new TextBox();
+            txtPassword = new TextBox();
+            ptbLoginIcono = new PictureBox();
+            btnAcceder = new Button();
+            btnSalir = new Button();
+            ((System.ComponentModel.ISupportInitialize)pcbLoginFondo).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ptbLoginIcono).BeginInit();
+            SuspendLayout();
+            // 
+            // pcbLoginFondo
+            // 
+            pcbLoginFondo.Image = (Image)resources.GetObject("pcbLoginFondo.Image");
+            pcbLoginFondo.Location = new Point(108, 85);
+            pcbLoginFondo.Name = "pcbLoginFondo";
+            pcbLoginFondo.Size = new Size(456, 500);
+            pcbLoginFondo.SizeMode = PictureBoxSizeMode.StretchImage;
+            pcbLoginFondo.TabIndex = 0;
+            pcbLoginFondo.TabStop = false;
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Font = new Font("Segoe UI", 12F);
+            txtUsuario.Location = new Point(715, 143);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.PlaceholderText = "Nombre del usuario";
+            txtUsuario.Size = new Size(238, 34);
+            txtUsuario.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Font = new Font("Segoe UI", 12F);
+            txtPassword.Location = new Point(715, 205);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PlaceholderText = "Contraseña del usuario";
+            txtPassword.Size = new Size(238, 34);
+            txtPassword.TabIndex = 2;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // ptbLoginIcono
+            // 
+            ptbLoginIcono.Image = (Image)resources.GetObject("ptbLoginIcono.Image");
+            ptbLoginIcono.Location = new Point(1064, 91);
+            ptbLoginIcono.Name = "ptbLoginIcono";
+            ptbLoginIcono.Size = new Size(229, 212);
+            ptbLoginIcono.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptbLoginIcono.TabIndex = 3;
+            ptbLoginIcono.TabStop = false;
+            // 
+            // btnAcceder
+            // 
+            btnAcceder.Font = new Font("Segoe UI", 12F);
+            btnAcceder.Location = new Point(715, 262);
+            btnAcceder.Name = "btnAcceder";
+            btnAcceder.Size = new Size(105, 41);
+            btnAcceder.TabIndex = 4;
+            btnAcceder.Text = "Acceder";
+            btnAcceder.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Font = new Font("Segoe UI", 12F);
+            btnSalir.Location = new Point(848, 262);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(105, 41);
+            btnSalir.TabIndex = 5;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1920, 991);
+            Controls.Add(btnSalir);
+            Controls.Add(btnAcceder);
+            Controls.Add(ptbLoginIcono);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsuario);
+            Controls.Add(pcbLoginFondo);
+            Name = "frmLogin";
+            Text = "Control de acceso";
+            WindowState = FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)pcbLoginFondo).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ptbLoginIcono).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pcbLoginFondo;
+        private TextBox txtUsuario;
+        private TextBox txtPassword;
+        private PictureBox ptbLoginIcono;
+        private Button btnAcceder;
+        private Button btnSalir;
+    }
+}
---
+++ frmPrincipal.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace prySistemaEscolar
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            pcbMenu = new PictureBox();
+            pcbTitulo = new PictureBox();
+            pcbAlumnos = new PictureBox();
+            pcbCarreras = new PictureBox();
+            pcbDocentes = new PictureBox();
+            pcbUsuarios = new PictureBox();
+            pcbSalir = new PictureBox();
+            pnlContenedor = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pcbMenu).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pcbTitulo).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pcbAlumnos).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pcbCarreras).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pcbDocentes).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pcbUsuarios).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pcbSalir).BeginInit();
+            SuspendLayout();
+            // 
+            // pcbMenu
+            // 
+            pcbMenu.Image = (Image)resources.GetObject("pcbMenu.Image");
+            pcbMenu.Location = new Point(12, 12);
+            pcbMenu.Name = "pcbMenu";
+            pcbMenu.Size = new Size(571, 956);
+            pcbMenu.SizeMode = PictureBoxSizeMode.StretchImage;
+            pcbMenu.TabIndex = 0;
+            pcbMenu.TabStop = false;
+            // 
+            // pcbTitulo
+            // 
+            pcbTitulo.Image = (Image)resources.GetObject("pcbTitulo.Image");
+            pcbTitulo.Location = new Point(645, 12);
+            pcbTitulo.Name = "pcbTitulo";
+            pcbTitulo.Size = new Size(1240, 153);
+            pcbTitulo.SizeMode = PictureBoxSizeMode.StretchImage;
+            pcbTitulo.TabIndex = 1;
+            pcbTitulo.TabStop = false;
+            // 
+            // pcbAlumnos
+            // 
+            pcbAlumnos.Image = (Image)resources.GetObject("pcbAlumnos.Image");
+            pcbAlumnos.Location = new Point(69, 169);
+            pcbAlumnos.Name = "pcbAlumnos";
+            pcbAlumnos.Size = new Size(434, 148);
+            pcbAlumnos.SizeMode = PictureBoxSizeMode.StretchImage;
+            pcbAlumnos.TabIndex = 2;
+            pcbAlumnos.TabStop = false;
+            pcbAlumnos.MouseDown += pcbGeneral_MouseDown;
+            pcbAlumnos.MouseUp += pcbGeneral_MouseUp;
+            // 
+            // pcbCarreras
+            // 
+            pcbCarreras.Image = (Image)resources.GetObject("pcbCarreras.Image");
+            pcbCarreras.Location = new Point(69, 323);
+            pcbCarreras.Name = "pcbCarreras";
+            pcbCarreras.Size = new Size(434, 148);
+            pcbCarreras.SizeMode = PictureBoxSizeMode.StretchImage;
+            pcbCarreras.TabIndex = 3;
+            pcbCarreras.TabStop = false;
+            pcbCarreras.MouseDown += pcbGeneral_MouseDown;
+            pcbCarreras.MouseUp += pcbGeneral_MouseUp;
+            // 
+            // pcbDocentes
+            // 
+            pcbDocentes.Image = (Image)resources.GetObject("pcbDocentes.Image");
+            pcbDocentes.Location = new Point(69, 477);
+            pcbDocentes.Name = "pcbDocentes";
+            pcbDocentes.Size = new Size(434, 148);
+            pcbDocentes.SizeMode = PictureBoxSizeMode.StretchImage;
+            pcbDocentes.TabIndex = 4;
+            pcbDocentes.TabStop = false;
+            pcbDocentes.MouseDown += pcbGeneral_MouseDown;
+            pcbDocentes.MouseUp += pcbGeneral_MouseUp;
+            // 
+            // pcbUsuarios
+            // 
+            pcbUsuarios.Image = (Image)resources.GetObject("pcbUsuarios.Image");
+            pcbUsuarios.Location = new Point(69, 631);
+            pcbUsuarios.Name = "pcbUsuarios";
+            pcbUsuarios.Size = new Size(434, 148);
+            pcbUsuarios.SizeMode = PictureBoxSizeMode.StretchImage;
+            pcbUsuarios.TabIndex = 5;
+            pcbUsuarios.TabStop = false;
+            pcbUsuarios.MouseDown += pcbGeneral_MouseDown;
+            pcbUsuarios.MouseUp += pcbGeneral_MouseUp;
+            // 
+            // pcbSalir
+            // 
+            pcbSalir.Image = (Image)resources.GetObject("pcbSalir.Image");
+            pcbSalir.Location = new Point(69, 785);
+            pcbSalir.Name = "pcbSalir";
+            pcbSalir.Size = new Size(434, 148);
+            pcbSalir.SizeMode = PictureBoxSizeMode.StretchImage;
+            pcbSalir.TabIndex = 6;
+            pcbSalir.TabStop = false;
+            pcbSalir.MouseDown += pcbGeneral_MouseDown;
+            pcbSalir.MouseUp += pcbGeneral_MouseUp;
+            // 
+            // pnlContenedor
+            // 
+            pnlContenedor.BackColor = Color.FromArgb(144, 194, 38);
+            pnlContenedor.Location = new Point(649, 191);
+            pnlContenedor.Name = "pnlContenedor";
+            pnlContenedor.Size = new Size(1236, 777);
+            pnlContenedor.TabIndex = 7;
+            // 
+            // frmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1920, 991);
+            Controls.Add(pnlContenedor);
+            Controls.Add(pcbSalir);
+            Controls.Add(pcbUsuarios);
+            Controls.Add(pcbDocentes);
+            Controls.Add(pcbCarreras);
+            Controls.Add(pcbAlumnos);
+            Controls.Add(pcbTitulo);
+            Controls.Add(pcbMenu);
+            Name = "frmPrincipal";
+            Text = "Sistema Escolar";
+            WindowState = FormWindowState.Maximized;
+            Load += frmPrincipal_Load;
+            ((System.ComponentModel.ISupportInitialize)pcbMenu).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pcbTitulo).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pcbAlumnos).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pcbCarreras).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pcbDocentes).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pcbUsuarios).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pcbSalir).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pcbMenu;
+        private PictureBox pcbTitulo;
+        private PictureBox pcbAlumnos;
+        private PictureBox pcbCarreras;
+        private PictureBox pcbDocentes;
+        private PictureBox pcbUsuarios;
+        private PictureBox pcbSalir;
+        private Panel pnlContenedor;
+    }
+}
