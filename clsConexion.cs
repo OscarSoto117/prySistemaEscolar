66516e234fd949848c46d5a90918fbc8dea8411e
Bryan Hafizi <bryan.hafizi.mx@gmail.com>
2026-06-10 07:58:15 -0600

    clase conexion

---
+++ clsConexion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prySistemaEscolar
+{
+    internal class clsConexion
+    {
+        private string host = "189.240.192.140";
+        private string db = "bdescuela3b";
+        private string usuario = "terceroB";
+        private string password = "tercero@B";
+        private string puerto = "3306";
+
+        private string cadenaConexion => $"server={host};database={db};user{usuario};password{password};port{puerto}";
+    }
+}
