209236dd53493b3d9767f8f9d4e9888023e262b2
Bryan Hafizi <bryan.hafizi.mx@gmail.com>
2026-06-11 11:23:39 -0600

    program

--- Program.cs
+++ Program.cs
@@ -11,13 +11,7 @@ namespace prySistemaEscolar
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-        using (frmLogin login = new frmLogin())
-        {
-            if (login.ShowDialog() == DialogResult.OK)
-            {
-                Application.Run(new frmPrincipal());
-            }
-        }
+        
         }
     }
 }
